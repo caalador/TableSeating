0ff2dce1478f480ebb743b51bbb6ce98160950d5
Mikael Grankvist <mikael.grankvist@vaadin.com>
2015-02-06 14:31:26 +0200

    Updated demo with new Image and random name generation.

--- Seating-demo/src/main/java/org/percepta/mgrankvi/demo/DemoUI.java
+++ Seating-demo/src/main/java/org/percepta/mgrankvi/demo/DemoUI.java
@@ -17,6 +17,8 @@ import org.percepta.mgrankvi.Table;
 import org.percepta.mgrankvi.client.TableSeatPlacing;
 import org.percepta.mgrankvi.client.contact.Contact;
 
+import java.util.Random;
+
 @Theme("demo")
 @Title("MyComponent Add-on Demo")
 @SuppressWarnings("serial")
@@ -27,31 +29,51 @@ public class DemoUI extends UI
     @VaadinServletConfiguration(productionMode = false, ui = DemoUI.class, widgetset = "org.percepta.mgrankvi.demo.DemoWidgetSet")
     public static class Servlet extends VaadinServlet {
     }
+    private static Random rand = new Random(System.currentTimeMillis());
 
     @Override
     protected void init(VaadinRequest request) {
 
         // Initialize our new UI component
-        final SeatingLayout component = new SeatingLayout("images/WaterTower.jpg");
-        component.setSizeFull();
+        final SeatingLayout component = new SeatingLayout("images/Room.png");
+        component.setWidth("1280px");
+        component.setHeight("1500px");
         int i = 0;
-        Table table = new Table(135, 130);
-        Contact c = new Contact("Perre Paras", null, Colours.cssColours[i++], "The sweetest of all sounds is that of the voice of the woman we love.", "Jean de la Bruyere");
-        Contact c1 = new Contact("Pelle peloton", null, Colours.cssColours[i++], "Where there is love there is life.", "Mahatma Gandhi");
-        Contact  c2 = new Contact("Fenix Fenkoli", null, Colours.cssColours[i++], "We loved with a love that was more than love.", "Edgar Allan Poe");
-        table.addContacts(c, c1, c2);
+        Table table = new Table(0, 130);
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
 
         table.setTableRotation(-20);
 
-        component.addComponent(table, new Position(200, 60));
+        component.addComponent(table, new Position(60, 200));
 
-        table = new Table(135, 130);
-        c = new Contact("Heidi", null, Colours.cssColours[i++], "We loved with a love that was more than love.", "Edgar Allan Poe");
-        c1 = new Contact("Mikael", null, Colours.cssColours[i++], "The sweetest of all sounds is that of the voice of the woman we love.", "Jean de la Bruyere");
-        table.addContacts(c, c1);
+        table = new Table(10, 130);
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+
+        table.setTableRotation(-20);
+
+        component.addComponent(table, new Position(60, 450));
+
+        table = new Table(0, 130);
+        table.addContact(new Contact("Bride", null, Colours.cssColours[i++], "We loved with a love that was more than love.", "Edgar Allan Poe"));
+        table.addContact(new Contact("Groom", null, Colours.cssColours[i++], "The sweetest of all sounds is that of the voice of the woman we love.", "Jean de la Bruyere"));
+
         table.setTableSeatPlacing(TableSeatPlacing.ALL_UP);
 
-        component.addComponent(table, new Position(1000, 60));
+        component.addComponent(table, new Position(635, 60));
 
         // Show it in the middle of the screen
         final VerticalLayout layout = new VerticalLayout();
@@ -61,6 +83,12 @@ public class DemoUI extends UI
         layout.setComponentAlignment(component, Alignment.MIDDLE_CENTER);
         setContent(layout);
 
+    }
+
+    public Contact createContact(int id) {
+        final String surName = Names.surNames[rand.nextInt(Names.surNames.length)];
+        final String firstName = Names.firstNames[rand.nextInt(Names.firstNames.length)];
+        return new Contact(firstName + " " + surName, null, Colours.cssColours[id], "", "");
     }
 
 }
