2dfc7bbf8324d0d01020fba5f2017cf62febe402
Mikael Grankvist <mikael.grankvist@vaadin.com>
2015-02-09 10:47:53 +0200

    Updated demo with server side search.

--- Seating-demo/src/main/java/org/percepta/mgrankvi/demo/DemoUI.java
+++ Seating-demo/src/main/java/org/percepta/mgrankvi/demo/DemoUI.java
@@ -1,13 +1,14 @@
 package org.percepta.mgrankvi.demo;
 
-import javax.servlet.annotation.WebServlet;
-
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Title;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 import org.percepta.mgrankvi.Colours;
@@ -17,29 +18,55 @@ import org.percepta.mgrankvi.Table;
 import org.percepta.mgrankvi.client.TableSeatPlacing;
 import org.percepta.mgrankvi.client.contact.Contact;
 
+import javax.servlet.annotation.WebServlet;
+import java.util.List;
 import java.util.Random;
 
 @Theme("demo")
 @Title("MyComponent Add-on Demo")
 @SuppressWarnings("serial")
-public class DemoUI extends UI
-{
+public class DemoUI extends UI {
 
     @WebServlet(value = "/*", asyncSupported = true)
     @VaadinServletConfiguration(productionMode = false, ui = DemoUI.class, widgetset = "org.percepta.mgrankvi.demo.DemoWidgetSet")
     public static class Servlet extends VaadinServlet {
     }
+
     private static Random rand = new Random(System.currentTimeMillis());
 
     @Override
     protected void init(VaadinRequest request) {
 
+
         // Initialize our new UI component
         final SeatingLayout component = new SeatingLayout("images/Room.png");
         component.setWidth("1280px");
         component.setHeight("1500px");
+
+        final TextField search = new TextField("Contact search");
+        search.setImmediate(true);
+        search.setWidth("125px");
+        Button searchButton = new Button("Search", new Button.ClickListener() {
+            @Override
+            public void buttonClick(Button.ClickEvent clickEvent) {
+                List<Contact> contact = component.findContact(search.getValue());
+                if (contact.size() == 1) {
+                    component.highlightContact(contact.get(0));
+                } else {
+                    StringBuilder sb = new StringBuilder();
+                    for (Contact c : contact) {
+                        sb.append(c.name).append("\n");
+                    }
+                    Notification.show("Multiple contacts found", sb.toString(), Notification.Type.HUMANIZED_MESSAGE);
+                }
+            }
+        });
+
+        component.addComponent(search, new Position(50, 50));
+        component.addComponent(searchButton, new Position(175, 50));
+
         int i = 0;
-        Table table = new Table(0, 130);
+        Table table = new Table(10, 130);
         table.addContact(createContact(i++));
         table.addContact(createContact(i++));
         table.addContact(createContact(i++));
@@ -67,13 +94,109 @@ public class DemoUI extends UI
 
         component.addComponent(table, new Position(60, 450));
 
-        table = new Table(0, 130);
-        table.addContact(new Contact("Bride", null, Colours.cssColours[i++], "We loved with a love that was more than love.", "Edgar Allan Poe"));
-        table.addContact(new Contact("Groom", null, Colours.cssColours[i++], "The sweetest of all sounds is that of the voice of the woman we love.", "Jean de la Bruyere"));
+        table = new Table(10, 130);
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+
+        table.setTableRotation(20);
+
+        component.addComponent(table, new Position(60, 750));
+
+        table = new Table(10, 130);
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+
+        table.setTableRotation(20);
+
+        component.addComponent(table, new Position(60, 1000));
+
+        table = new Table(10, 130);
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+
+        table.setTableRotation(20);
+
+        component.addComponent(table, new Position(890, 200));
+
+        table = new Table(10, 130);
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+
+        table.setTableRotation(20);
+
+        component.addComponent(table, new Position(890, 450));
+
+        table = new Table(10, 130);
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+
+        table.setTableRotation(-20);
+
+        component.addComponent(table, new Position(890, 750));
+
+        table = new Table(10, 130);
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+        table.addContact(createContact(i++));
+
+        table.setTableRotation(-20);
+
+        component.addComponent(table, new Position(890, 1000));
+
+        table = new Table(2, 130);
+        table.addContact(new Contact(Contact.uuid(), "Bride", null, Colours.cssColours[i++], "We loved with a love that was more than love.", "Edgar Allan Poe"));
+        table.addContact(new Contact(Contact.uuid(), "Groom", null, Colours.cssColours[i++], "The sweetest of all sounds is that of the voice of the woman we love.", "Jean de la Bruyere"));
 
         table.setTableSeatPlacing(TableSeatPlacing.ALL_UP);
 
-        component.addComponent(table, new Position(635, 60));
+        component.addComponent(table, new Position(560, 80));
 
         // Show it in the middle of the screen
         final VerticalLayout layout = new VerticalLayout();
@@ -88,7 +211,7 @@ public class DemoUI extends UI
     public Contact createContact(int id) {
         final String surName = Names.surNames[rand.nextInt(Names.surNames.length)];
         final String firstName = Names.firstNames[rand.nextInt(Names.firstNames.length)];
-        return new Contact(firstName + " " + surName, null, Colours.cssColours[id], "", "");
+        return new Contact(Contact.uuid(), firstName + " " + surName, null, Colours.cssColours[id], "", "");
     }
 
 }
